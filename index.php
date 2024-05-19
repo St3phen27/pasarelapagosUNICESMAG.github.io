fc209aa725fd9788b563fedb65472650a128011a
Esteban Erazo <narvaezerazo@hotmail.com>
2024-05-19 00:37:35 -0500

    avance 3

--- index.php
+++ index.php
@@ -3,54 +3,61 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <script src="https://www.paypal.com/sdk/js?client-id=AS53jKQEm9zvX8Mcp698FS-gFIsZ4NDBnss1kF0QyKq57vJ2FTkrg5977MWCZiJrHrBM9frl46N13Es5&currency=MXN">
-    </script>
+    <title>Home</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" 
+    rel="stylesheet" 
+    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" 
+    crossorigin="anonymous">
 </head>
 <body>
-    <div id="paypal-button-container"></div>
-    <script>
-        paypal.Buttons({
-            style:{
-                color: 'blue',
-                shape: 'pill',
-                label: 'pay'
-            },
-            createOrder: function(data, actions){
-                return actions.order.create({
-                    purchase_units: [{
-                        amount: {
-                            value: 100
-                        }
-                    }]
-                });
-            },
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" 
+    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" 
+    crossorigin="anonymous"></script>
 
-            onApprove: function(data, actions){
-                actions.order.capture().then(function(detalles){
-                    console.log(detalles);
-                    window.location.href="order-information.html"
-                });
-            },
+    <header>
+    <div class="navbar navbar-exapand-lg navbar-dark bg-dark">
+        <div class="container">
+        <a href="#" class="navbar-brand">
+            <strong>Tienda CESMAG</strong>
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" 
+        data-bs-target="#navbarHeader" aria-controls="navbarHeader" 
+        aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarHeader">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a href="#" calss="nav-link active">Catalogo</a>
+                </li>
+            </ul>
+            <a href="cart.php" class="btn btn-primary">Carrito</a>
+        </div> 
+        </div>
+    </div>
+    </header>
 
-            onCancel: function(data){
-                alert("Pago cancelado");
-                console.log(data);
-            }
-        }).render('#paypal-button-container');
-    </script>
-    
-    
-
-    <form action="https://checkout.wompi.co/p/" method="GET">
-      <input type="hidden" name="public-key" value="pub_test_501zz4iE1TnIknS0Ob5DEs22JFFVaxYe" />
-      <input type="hidden" name="currency" value="COP" />
-      <input id="totalPay" type="hidden" name="amount-in-cents" value="100"/>
-      <input id="reference" type="hidden" name="reference" value="referencia_pago_web_checkout_ejemplo_01" />
-      <input type="hidden" name="redirect-url" value="https://google.com" />
-      <button class="checkPageBtn" type="submit">COMPRAR AHORA</button>
-      </form>
-
+<main>
+    <div class="container">
+    <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+        <div class="col">
+          <div class="card shadow-sm">
+            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+            <div class="card-body">
+              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                  <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                  <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                </div>
+                <small class="text-body-secondary">9 mins</small>
+              </div>
+            </div>
+          </div>
+        </div>
+    </div>
+    </div>
+</main>
 
 </body>
 </html>
